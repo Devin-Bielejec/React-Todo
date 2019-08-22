2b9eeb14b3180c5a20b4eef3e991ca5464ffb63d
Devin Bielejec <devin.bielejec@gmail.com>
2019-08-21 21:24:50 -0400

    Added hover strikethrough to list items

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,8 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
+const ToDoItem = styled.li`
+    &:hover{
+        text-decoration: line-through;
+    }
+`
 const ToDoComponent = (props) => {
     return (
-        <li key={props.toDoItem.id} completed={props.toDoItem.completed.toString()}>{props.toDoItem.task}</li>
+        <ToDoItem key={props.toDoItem.id} completed={props.toDoItem.completed.toString()}>{props.toDoItem.task}</ToDoItem>
     )
 }
 
