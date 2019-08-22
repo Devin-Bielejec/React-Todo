33fab2ac6f9b1e365267019ca18d501c08ef45be
devin-bielejec <devin.bieleje@gmail.com>
2019-08-22 17:55:39 -0400

    Toggled correctly for list item

--- src/App.js
+++ src/App.js
@@ -20,16 +20,25 @@ class App extends React.Component {
     )
   }
 
-  removeToDo = (event) => {
+  toggleCompleted = (event) => {
     const currentId = event.target.id;
-    //Shows the item as strucken through
-    event.target.style.textDecoration = "line-through";
-    console.log(this.state.toDo);
+    
+    //Toggle line-through
+    let completedValue = true;
+    console.log(event.target.style.textDecoration === "line-through");
+    if (event.target.style.textDecoration !== "line-through") {
+      let completedValue = true;
+      event.target.style.textDecoration = "line-through";
+    } else {
+      let completedValue = true;
+      event.target.style.textDecoration = "none";
+    }
+
     this.setState(function () {
       //Find object in state
       let currentToDoItem = this.state.toDo.filter( obj => obj.id.toString() === currentId );
       console.log(currentToDoItem);
-      currentToDoItem[0].completed = true;
+      currentToDoItem[0].completed = completedValue;
       console.log("This state inside of setState is", this.state);
       return this.state;
     })
@@ -65,7 +74,7 @@ class App extends React.Component {
         <FormComponent clearCompleted={this.clearCompleted}
         onInputChange={this.onInputChange}
         addToDo={this.addToDo}/>
-        <ToDoList toDoList={this.state.toDo} removeToDo={this.removeToDo}/>
+        <ToDoList toDoList={this.state.toDo} toggleCompleted={this.toggleCompleted}/>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -7,7 +7,7 @@ const ToDoList = (props) => {
     return(
         <ul>
             {props.toDoList.map( toDoItem => {
-                return (<ToDoComponent toDoItem={toDoItem} removeToDo={props.removeToDo}/>)
+                return (<ToDoComponent toDoItem={toDoItem} toggleCompleted={props.toggleCompleted}/>)
             })}
         </ul>
     )
